685fcfce2fb7c5b317884a565c80a19b4134ac72
vladislav7martin <58507269+vladislav7martin@users.noreply.github.com>
2024-08-21 17:30:58 +0200

    Update streamlit_app.py
    CONNECTED CHANGES NEEDED BC OF MOVE FROM SIS TO SNIS

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
 """Choose the fruits you want in your custom Smoothie!
 """)
 
-
+# my Changes
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
